f2a1f68cf07b0e93adb1fe6640d96fdb1bb3f883
Lenik <pub.lenik@bodz.net>
2026-06-17 20:02:20 +0800

    Expand oremind bash completion for new themes and flags.
    Complete all theme names, add --diag, and support key=value option
    completion for --locale and --theme.

--- oremind.bash
+++ oremind.bash
@@ -1,5 +1,9 @@
 # bash completion for oremind
 
+_oremind_locales='en zh_CN zh_TW ja ko th vi fr de it eo'
+_oremind_themes='dark light innocent maiden girl morandi github ios msdos windows'
+_oremind_opts='-v --verbose -q --quiet -h --help -l --locale -t --theme -o --opacity -c --cancel -i --interval -w --weekstart -d --database --diag --version'
+
 _oremind()
 {
 	local cur prev words cword
@@ -7,22 +11,14 @@ _oremind()
 
 	case "$prev" in
 		-l|--locale)
-			COMPREPLY=($(compgen -W 'en zh_CN zh_TW ja ko th vi fr de it eo' -- "$cur"))
+			COMPREPLY=($(compgen -W "$_oremind_locales" -- "$cur"))
 			return
 			;;
 		-t|--theme)
-			COMPREPLY=($(compgen -W 'dark light' -- "$cur"))
+			COMPREPLY=($(compgen -W "$_oremind_themes" -- "$cur"))
 			return
 			;;
-		-o|--opacity)
-			COMPREPLY=()
-			return
-			;;
-		-c|--cancel)
-			COMPREPLY=()
-			return
-			;;
-		-i|--interval)
+		-o|--opacity|-c|--cancel|-i|--interval)
 			COMPREPLY=()
 			return
 			;;
@@ -34,10 +30,38 @@ _oremind()
 			_filedir
 			return
 			;;
+		--diag|--version|-h|--help|-v|--verbose|-q|--quiet)
+			COMPREPLY=()
+			return
+			;;
 	esac
 
+	if [[ "$cur" == *=* ]]; then
+		local key="${cur%%=*}" val="${cur#*=}"
+		case "$key" in
+			--locale|-l)
+				mapfile -t COMPREPLY < <(compgen -W "$_oremind_locales" -- "$val")
+				;;
+			--theme|-t)
+				mapfile -t COMPREPLY < <(compgen -W "$_oremind_themes" -- "$val")
+				;;
+			*)
+				COMPREPLY=()
+				;;
+		esac
+		if ((${#COMPREPLY[@]})); then
+			local reply
+			reply=()
+			for item in "${COMPREPLY[@]}"; do
+				reply+=("${key}=${item}")
+			done
+			COMPREPLY=("${reply[@]}")
+		fi
+		return
+	fi
+
 	if [[ $cur == -* ]]; then
-		COMPREPLY=($(compgen -W '-v --verbose -q --quiet -h --help -l --locale -t --theme -o --opacity -c --cancel -i --interval -w --weekstart -d --database --version' -- "$cur"))
+		COMPREPLY=($(compgen -W "$_oremind_opts" -- "$cur"))
 		return
 	fi
 
